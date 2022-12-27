ffbddec21cc8cba9d11fdcc8f32f9e2375298376
mauribarbona <116315725+mauribarbona@users.noreply.github.com>
2022-12-26 22:41:14 -0300

    Add files via upload

--- carrousel.js
+++ carrousel.js
@@ -44,4 +44,4 @@ for (i = 0; i < 256; columns[i++] = 1);
 function step() {
     canvas.getContext('2d').fillStyle = 'rgba(0,0,0,0.05)';
     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height);
-    canvas.getContext('2d').fillStyle = '#0F0';
+    canvas.getContext('2d').fillStyle = '#0F0';}
